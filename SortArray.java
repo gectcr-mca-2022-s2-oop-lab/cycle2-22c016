38c00c681cfa26f59e183fc43fb6db699a235915
22c016 <22c016@gectcr.ac.in>
2023-03-24 12:13:11 +0530

    Array Sort

---
+++ SortArray.java
@@ -0,0 +1,50 @@
+package cycle2;
+import java.util.*;
+
+public class SortArray {
+
+	public static void main(String[] args) {
+
+		int num,i ;
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter size of array: ");
+		num= in.nextInt();
+		String arrays[]=new String[num];
+		System.out.println("Enter elements of array: ");
+		for(i=0;i<num;i++)
+		{
+			arrays[i]=in.next();
+		}
+		System.out.println("Original array: ");
+		System.out.println();
+		for(i=0;i<num;i++)
+		{
+			System.out.print(arrays[i] + " ");
+		}
+		System.out.println();
+		System.out.println("Sorted array: ");
+		System.out.println();
+		for(i=0;i<num;i++)
+		{
+			for(int j=i+1;j<num;j++)
+			{
+				if(arrays[i].compareTo(arrays[j])>0)
+	                {  
+	                    
+                    String temp = arrays[i];  
+	                    arrays[i] = arrays[j];  
+	                    arrays[j] = temp;  
+	                 }  
+			}
+		}
+		
+		for(i=0;i<num;i++)
+		{
+			System.out.print(arrays[i] + " ");
+		}
+
+	}
+
+}
+   
+   
