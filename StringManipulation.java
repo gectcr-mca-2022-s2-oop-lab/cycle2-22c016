6db23a8a535fde1c98461f8110bb351d1d205d4b
22c016 <22c016@gectcr.ac.in>
2023-03-30 14:28:53 +0530

    StringMethods

--- StringManipulation.java
+++ StringManipulation.java
@@ -1,48 +1,50 @@
 package cycle2;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+public class StringManipulation {
 
-class StringManipulation{
-    String str;
-	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the string");
+		String str=sc.next();
+		System.out.println("String Methods");
+		int choice;
+		do {
+			System.out.println("1.UpperCase");
+			System.out.println("2.LowerCase");
+			System.out.println("3.Length");
+			System.out.println("4.Replace");
+			System.out.println("5.Concat");
+			System.out.println("6.Equals");
+			System.out.println("0.Exit");
+			System.out.println("Enter your choice");
+			choice=sc.nextInt();
+			switch(choice) {
+			case 1:System.out.println(str.toUpperCase());
+					break;
+			case 2:System.out.println(str.toLowerCase());
+					break;
+			case 3:System.out.println("length="+str.length());
+			break;
+			case 4:System.out.println("Enter the character to be replaced");
+					char c=sc.next().charAt(0);
+					System.out.println("Enter the new character ");
+					char ch=sc.next().charAt(0);
+					System.out.println(str.replace(c, ch));
+					break;
+			case 5:System.out.println("Enter the string to be appended");
+					String s=sc.next();
+					System.out.println(str.concat(s));
+					break;
+			case 6:System.out.println("Enter the second string");
+			String s1=sc.next();
+			System.out.println(str.equals(s1));
+			break;
+			
+			case 0:break;
+			}
+		}
+			while(choice!=0);
+		
+	}
 
-
-    StringManipulation(String str) {
-        this.str = str;
-    }
-
-    void manipulationMethods(){
-        String value = this.str;
-        System.out.println( " String Length is : " + value.length());
-        
-        System.out.println(" Enter an alphabet to find its index:");
-        System.out.println("Index: " + value.indexOf(sc.next()));
-        
-        System.out.println(" Enter the position to find the character there:");
-        System.out.println("Character at position: " + value.charAt(sc.nextInt()));
-        
-        System.out.println(" Enter a string to concatenate with input:");
-
-        System.out.println("Concatenating 'hello' with input : " +value.concat(sc.next()));
-        
-        System.out.println(" enter a string to compare with input:");
-        System.out.println("Comparison: " + value.compareTo(sc.next()));
-        
-        System.out.println(" enter a string to check if it is equal to input:");
-        System.out.println("equals?  " + value.equals(sc.next()));
-        
-        System.out.println("Enter a string to check if it is contained in the input:");
-        System.out.println("Contains ? "+ value.contains(sc.next()));
-
-
-        
-        System.out.println( "Conversion to LowerCase: " + value.toLowerCase());
-        System.out.println( "Conversion to UpperCase: " + value.toUpperCase());
-    }
-
-    public static void main(String []args) {
-    	System.out.println("Enter a string:");
-    	StringManipulation str = new StringManipulation(sc.nextLine());
-    	str.manipulationMethods();
-    }
 }
